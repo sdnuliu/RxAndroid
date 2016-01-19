5a8f840e8d6f03686c5d2f2d388f0aa7a3352fa4
liuteng <sdnuliu@qq.com>
2016-01-19 16:10:19 +0800

    add rxandroid test

--- sample-app/src/main/java/rx/android/samples/MainActivity.java
+++ sample-app/src/main/java/rx/android/samples/MainActivity.java
@@ -6,13 +6,20 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.android.schedulers.HandlerScheduler;
 import rx.exceptions.OnErrorThrowable;
+import rx.functions.Action0;
 import rx.functions.Func0;
+import rx.subjects.PublishSubject;
 
 import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
 
@@ -20,8 +27,10 @@ public class MainActivity extends Activity {
     private static final String TAG = "RxAndroidSamples";
 
     private Handler backgroundHandler;
+    private Observable<Integer> integerObservable;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
@@ -31,8 +40,123 @@ public class MainActivity extends Activity {
         backgroundHandler = new Handler(backgroundThread.getLooper());
 
         findViewById(R.id.button_run_scheduler).setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 onRunSchedulerExampleButtonClicked();
+            }
+        });
+        //简单的RxAndroid测试
+        findViewById(R.id.btn_next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRunSimpleRxAndroidTest();
+            }
+        });
+        //已有数据的RxAndroid测试
+        findViewById(R.id.btn_next2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRunExistDataButtonClick();
+            }
+        });
+        //PublishSubject测试
+        findViewById(R.id.btn_next3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRunSubjectTest();
+            }
+        });
+    }
+
+    private void onRunSubjectTest() {
+        final PublishSubject<Boolean> objectPublishSubject = PublishSubject.create();
+        objectPublishSubject.subscribe(new Observer<Boolean>() {
+            @Override
+            public void onCompleted() {
+                Toast.makeText(MainActivity.this, "Observable completed!!And PublishSubject " +
+                        "completed!!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(Boolean aBoolean) {
+                Toast.makeText(MainActivity.this, aBoolean + "", Toast.LENGTH_SHORT)
+                        .show();
+//                onCompleted();
+            }
+        });
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i < 5; i++) {
+                    subscriber.onNext(i);
+                }
+                subscriber.onCompleted();
+            }
+        }).doOnCompleted(new Action0() {
+            @Override
+            public void call() {
+                objectPublishSubject.onNext(true);
+            }
+        }).subscribe();
+    }
+
+    private void onRunExistDataButtonClick() {
+        ArrayList strList = new ArrayList();
+        strList.add("hello");
+        strList.add("world");
+        strList.add("sdnuliu");
+        Observable fromObservable = Observable.from(strList);
+        fromObservable.subscribe(new Observer() {
+            @Override
+            public void onCompleted() {
+                Toast.makeText(MainActivity.this, "onCompleted!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(MainActivity.this, "onError!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Toast.makeText(MainActivity.this, "Item is:" + o.toString(), Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+
+
+    private void onRunSimpleRxAndroidTest() {
+        integerObservable = Observable.create(new Observable.OnSubscribe<Integer>() {
+
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i < 5; i++) {
+                    subscriber.onNext(i);
+                }
+                subscriber.onCompleted();
+            }
+        });
+        integerObservable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onCompleted() {
+                Toast.makeText(MainActivity.this, "onCompleted!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Toast.makeText(MainActivity.this, "onError!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Toast.makeText(MainActivity.this, "onNext(" + integer + ")", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -44,15 +168,18 @@ public class MainActivity extends Activity {
                 // Be notified on the main thread
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<String>() {
-                    @Override public void onCompleted() {
+                    @Override
+                    public void onCompleted() {
                         Log.d(TAG, "onCompleted()");
                     }
 
-                    @Override public void onError(Throwable e) {
+                    @Override
+                    public void onError(Throwable e) {
                         Log.e(TAG, "onError()", e);
                     }
 
-                    @Override public void onNext(String string) {
+                    @Override
+                    public void onNext(String string) {
                         Log.d(TAG, "onNext(" + string + ")");
                     }
                 });
@@ -60,7 +187,8 @@ public class MainActivity extends Activity {
 
     static Observable<String> sampleObservable() {
         return Observable.defer(new Func0<Observable<String>>() {
-            @Override public Observable<String> call() {
+            @Override
+            public Observable<String> call() {
                 try {
                     // Do some long running operation
                     Thread.sleep(TimeUnit.SECONDS.toMillis(5));
@@ -68,6 +196,7 @@ public class MainActivity extends Activity {
                     throw OnErrorThrowable.from(e);
                 }
                 return Observable.just("one", "two", "three", "four", "five");
+                //just
             }
         });
     }
